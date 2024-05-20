15dd36d8b4657f5ea6fd34e866e2f281ecb33284
CamilaLinethPro <camila@prointernet.mx>
2024-05-20 15:56:26 -0600

    Primer avance
    aaa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './platzi.webp';
-import './App.css';
+
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+import React from 'react';
+
+const defaultTodos = [
+  {text: 'Cortar cebolla', completed: true},
+  {text: 'Tomar el curso de Intro a React.js', completed: false}, 
+  {text: 'LLorar con la llorona', completed: false}, 
+  {text: 'Estar con mi amorcito', completed: false},
+];
 
 function App() {
+  cons [todos, seTtodos] = React.useState();
+  const [searchValue, setSearchValue] = React.useState('');
+  console.log('Los usuarios buscan TODOS de ' + searchValue);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoCounter completed = {16} total={25} />
+      <TodoSearch 
+      searchValue = {searchValue}
+      setSearchValue = {setSearchValue}
+      />
+
+      <TodoList>
+        {defaultTodos.map(todo => (
+          <TodoItem 
+          key={todo.text} 
+          text={todo.text}
+          completed = {todo.completed}/>
+        ))}
+        
+      </TodoList>
+
+      <CreateTodoButton/>
+      
+    </>
   );
 }
+
+
+
+
 
 export default App;
